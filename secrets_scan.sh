9e9d43a1434deec7c5442cb11e222923f37914ef
qmixxx <evdoshenko.mn@gmail.com>
2026-03-16 14:56:37 +0100

    workflow changes

--- secrets_scan.sh
+++ secrets_scan.sh
@@ -9,12 +9,21 @@ echo "Starting the scanner..."
 # -n: показать номер строки
 # -E: использовать расширенные регулярные выражения
 # -i: игнорировать регистр
-grep -rniE "(password|api[_-]?key|secret|token|credential)" "$SEARCH_PATH" \
+RESULTS = $(grep -rniE "(password|api[_-]?key|secret|token|credential)" "$SEARCH_PATH" \
   --include=\*.{env,conf,yaml,yml,json} \
   --exclude-dir={.git,node_modules,.github,target} |
 grep -v '\${{' |
 grep -v '\${[A-Z_]*}' |
 grep -v 'example'
+)
+
+if [ -z "$RESULTS"]; then
+  echo "No secrets found."
+else
+  echo "Potential secrets detected:"
+  echo "$RESULT"
+  exit 1
+fi
 
 echo "Scanning is completed..."
 
