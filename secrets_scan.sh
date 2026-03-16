a0bf85a9f5f58bd7541dac77b397f02699549bd4
qmixxx <evdoshenko.mn@gmail.com>
2026-03-16 15:28:34 +0100

    workflow changes

--- secrets_scan.sh
+++ secrets_scan.sh
@@ -2,14 +2,14 @@
 
 SEARCH_PATH=${1:-.}
 
-echo "Starting the scanner..."
+echo "🕵️ Starting secret scanner..."
 
 # Ищем ключевые слова в файлах конфигурации (.env, .conf, .yaml, .yml, .json)
 # -r: рекурсивно
 # -n: показать номер строки
 # -E: использовать расширенные регулярные выражения
 # -i: игнорировать регистр
-RESULTS = $(grep -rniE "(password|api[_-]?key|secret|token|credential)" "$SEARCH_PATH" \
+RESULT = $(grep -rniE "(password|api[_-]?key|secret|token|credential)" "$SEARCH_PATH" \
   --include=\*.{env,conf,yaml,yml,json} \
   --exclude-dir={.git,node_modules,.github,target} |
 grep -v '\${{' |
@@ -17,13 +17,22 @@ grep -v '\${[A-Z_]*}' |
 grep -v 'example'
 )
 
-if [ -z "$RESULTS"]; then
-  echo "No secrets found."
+if [ -z "$RESULT" ]; then
+    echo "✅ No secrets found."
 else
-  echo "Potential secrets detected:"
-  echo "$RESULT"
-  exit 1
+    echo "❌ Potential secrets detected:"
+    echo "$RESULT"
+
+    # GitHub Actions annotations
+    while IFS= read -r line; do
+        file=$(echo "$line" | cut -d: -f1)
+        lineno=$(echo "$line" | cut -d: -f2)
+        message=$(echo "$line" | cut -d: -f3-)
+        echo "::error file=$file,line=$lineno::$message"
+    done <<< "$RESULT"
+
+    exit 1
 fi
 
-echo "Scanning is completed..."
+echo "🛡️ Scan completed."
 
